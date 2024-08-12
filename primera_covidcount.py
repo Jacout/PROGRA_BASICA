5a633372d35e58eedc206ae93978a3cee5f74958
Jacob Rodriguez <148935424+Jacout@users.noreply.github.com>
2024-08-11 23:06:04 -0600

    primera pre version covidcount

---
+++ primera_covidcount.py
@@ -0,0 +1,19 @@
+print("Bienvenido a CoviCout")
+
+
+print("1. Consultar casos de COVID")
+print("2. Consultar recuperaciones de COVID")
+print("3. Consultar defunciones de COVID")
+
+opcion = input("Ingrese el número de la opción que desea consultar: ")
+if opcion == "1":
+    # Código para consultar casos de COVID
+    pass
+elif opcion == "2":
+    # Código para consultar recuperaciones de COVID
+    pass
+elif opcion == "3":
+    # Código para consultar defunciones de COVID
+    pass
+else:
+    print("Opción inválida. Por favor, ingrese un número válido.")
